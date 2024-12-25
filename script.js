2e1ac167f8964cb67dcf726aa3ee363f90b770fe
Amr Noorwali <amr_a.h1424@hotmail.com>
2024-12-25 19:43:01 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+var chstate=()=> {
+    let num1=parseFloat( document.getElementById("num1").value ); 
+    let num2=parseFloat( document.getElementById("num2").value ); 
+
+    let sum=document.getElementById("sum");
+
+    sum.innerHTML=num1+num2;
+
+
+
+}
